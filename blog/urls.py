bbd4270cbadfd29675dce97367bf0df5aee7a477
İbrahim EDİZ <44032568+ibrahimediz@users.noreply.github.com>
2021-02-03 21:43:55 +0300

    03022021

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.gonderilistele,name="gonderilistele"), # /blog/
+    path('detay/<int:pk>',views.gonderidetay,name="gonderidetay"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import GonderiModel
 
 def gonderilistele(request): # => Function Based View
@@ -6,3 +6,8 @@ def gonderilistele(request): # => Function Based View
     return render(request,"blog/listele.html",{"gonderis":gonderiler})
     #             istek,templateadres,modeldengelenbilgi
 
+
+
+def gonderidetay(request,pk):
+    gonderi = get_object_or_404(GonderiModel,pk=pk)
+    return render(request,"blog/detay.html",{"gonderi":gonderi})
